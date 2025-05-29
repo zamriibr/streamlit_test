576e001f195b2bd4a8422a56930b43614493c1be
zamriibr <48670145+zamriibr@users.noreply.github.com>
2025-05-29 13:20:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,26 @@
 import streamlit as st 
+import requests
+
 # Set the app title 
 st.title('My First Streamlit App !!') 
+
 # Add a welcome message 
 st.write('Welcome to my Streamlit app!') 
+
 # Create a text input 
 widgetuser_input = st.text_input('Enter a custom message:', 'Hello, Streamlit!') 
+
 # Display the customized message 
 st.write('Customized Message:', widgetuser_input)
+
+
+#API calls
+responds = requests.get('https://api.vatcomply.com/rates?base=USD')
+
+# Add text
+st.write('Respond:') 
+
+# Display the customized message 
+st.write('Output:', responds)
+
+
