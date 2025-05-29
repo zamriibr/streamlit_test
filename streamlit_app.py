c26f007fdd412c29886c09a497118d3ca1b8f4a9
zamriibr <48670145+zamriibr@users.noreply.github.com>
2025-05-29 13:08:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 # Set the app title 
-st.title('My First Streamlit App') 
+st.title('My First Streamlit App !!') 
 # Add a welcome message 
 st.write('Welcome to my Streamlit app!') 
 # Create a text input 
